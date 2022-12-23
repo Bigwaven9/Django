a7ea3266458b7b887fd5bd380e8091d0f58ab897
Bigwave <bigwavelin@gmail.com>
2022-12-24 01:25:52 +0800

    t

--- acapp/urls.py
+++ acapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('game/', include('game.urls')),
+    path('edward/', include('game.urls')),
     path('admin/', admin.site.urls),
 ]
