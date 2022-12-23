f0e58c8930af6ffb881e77ab00849d867e33a6d3
Bigwave <jiahui9@illinois.edu>
2022-12-23 11:35:37 -0600

    1

--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from game.views import *
-
-urlpatterns = [
-    path("", index, name = "index"),
-    path("1", edward, name = "edward"),
-    path("play/", play, name = "play")
-]
