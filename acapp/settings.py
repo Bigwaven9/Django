e60a859953919255e4ee7c7028fe166ff7b82eb9
Bigwave <jiahui9@illinois.edu>
2022-12-23 11:43:26 -0600

    add

--- acapp/settings.py
+++ acapp/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'GMT-6'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
