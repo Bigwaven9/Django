bdfd520062419356245e16b96f99b86276695c81
Bigwave <jiahui9@illinois.edu>
2022-12-23 11:32:13 -0600

    1

--- game/urls.py
+++ game/urls.py
@@ -4,5 +4,5 @@ from game.views import *
 urlpatterns = [
     path("", index, name = "index"),
     path("1", edward, name = "edward"),
-    path("play/", play, name = "play"),
+    path("play/", play, name = "play")
 ]
