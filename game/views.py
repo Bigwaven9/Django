a489d00643107ec7e6afa0c850fde2fbf9bd8d2a
Bigwave <bigwavelin@gmail.com>
2022-12-23 13:35:14 +0800

    game update

--- game/views.py
+++ game/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is my first website! * 2 + '\n'")
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    return HttpResponse(line1)
