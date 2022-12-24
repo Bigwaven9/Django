4fda8b6482c350ac67e320369295267a51a3b6b8
Bigwave <jiahui9@illinois.edu>
2022-12-23 18:47:07 -0600

    a

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,25 +1,28 @@
 class AcGameMenu {
     constructor(root) {
-        this.root  = root;
+        this.root = root;
         this.$menu = $(`
 <div class="ac-game-menu">
     <div class="ac-game-menu-field">
-        <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
             单人模式
         </div>
-        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi">
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
             多人模式
         </div>
+        <br>
         <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
-            设置
+            退出
         </div>
     </div>
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
-        this.$single = this.$menu.find('.ac-game-menu-field-item-single');
-        this.$single = this.$menu.find('.ac-game-menu-field-item-multi');
-        this.$single = this.$menu.find('.ac-game-menu-field-item-settings');
+        this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
+
     }
 }class AcGame{
     constructor(id) {
